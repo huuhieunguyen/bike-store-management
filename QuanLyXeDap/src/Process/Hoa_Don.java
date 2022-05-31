e92a72c4ace0e94a1b25353394f5364ab120d0cd
NguyenHuuHieu <huuhieu.nguyen0408@gmail.com>
2022-05-31 19:04:01 +0700

    Hoa Don

---
+++ QuanLyXeDap/src/Process/Hoa_Don.java
@@ -0,0 +1,90 @@
+
+package Process;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Nguyen Huu Hieu
+ */
+public class Hoa_Don {
+    // Tao List Hoa Don
+    private static List<String> Hoa_Don = new ArrayList<>();
+    // Them cac thuoc tinh (phan tu) vao List Hoa Don
+    public static List<String> getData(){
+        Hoa_Don.add("MÃ HÓA ĐƠN");
+        Hoa_Don.add("NGÀY HÓA ĐƠN");
+        Hoa_Don.add("MÃ KHÁCH HÀNG");
+        Hoa_Don.add("MÃ NHÂN VIÊN");
+        Hoa_Don.add("TRỊ GIÁ");
+        Hoa_Don.add("MÃ KHUYẾN MÃI");
+        
+        return Hoa_Don;
+    }
+    
+    private String maHD, ngHD, maKH, maNV, maKM;
+    private float triGia;
+
+    public Hoa_Don() {
+    }
+
+    public Hoa_Don(String maHD, String ngHD, String maKH, String maNV, String maKM, float triGia) {
+        this.maHD = maHD;
+        this.ngHD = ngHD;
+        this.maKH = maKH;
+        this.maNV = maNV;
+        this.maKM = maKM;
+        this.triGia = triGia;
+    }
+
+    public String getMaHD() {
+        return maHD;
+    }
+
+    public void setMaHD(String maHD) {
+        this.maHD = maHD;
+    }
+
+    public String getNgHD() {
+        return ngHD;
+    }
+
+    public void setNgHD(String ngHD) {
+        this.ngHD = ngHD;
+    }
+
+    public String getMaKH() {
+        return maKH;
+    }
+
+    public void setMaKH(String maKH) {
+        this.maKH = maKH;
+    }
+
+    public String getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+
+    public String getMaKM() {
+        return maKM;
+    }
+
+    public void setMaKM(String maKM) {
+        this.maKM = maKM;
+    }
+
+    public float getTriGia() {
+        return triGia;
+    }
+
+    public void setTriGia(float triGia) {
+        this.triGia = triGia;
+    }
+    
+    
+}
---
+++ QuanLyXeDap/src/Process/KhachHang.java
@@ -0,0 +1,26 @@
+
+package Process;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Nguyen Huu Hieu
+ */
+public class KhachHang {
+    // Tao List Khach Hang
+    private static List<String> KhachHang = new ArrayList<>();
+    // Them cac thuoc tinh (phan tu) vao List Khach Hang
+    public static List<String> getDataKH(){
+        KhachHang.add("MÃ KHÁCH HÀNG");
+        KhachHang.add("HỌ TÊN");
+//        KhachHang.add("ĐỊA CHỈ");
+//        KhachHang.add("GIỚI TÍNH");
+        KhachHang.add("SĐT");
+//        KhachHang.add("NGÀY SINH");
+        
+        return KhachHang;
+    }
+    
+}
